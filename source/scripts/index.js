ee7ad8c61dd0697a7382afb54181ddde29dbef39
Oksana Lanovenko <okslan@gmail.com>
2025-01-26 21:32:08 +0400

    26/01 (1)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,12 @@
 /* в этот файл добавляет скрипты*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.querySelector('.user-nav__toggle');
+  const navList = document.querySelector('.main-nav');
+
+  toggleButton.addEventListener('click', () => {
+    toggleButton.classList.toggle('user-nav__toggle--open');
+    toggleButton.classList.toggle('user-nav__toggle--close');
+    navList.classList.toggle('main-nav--opened');
+  });
+});
