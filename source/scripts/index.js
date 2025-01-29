e704eb320dc9912f658a41a119e27f67a6e369eb
Oksana Lanovenko <158132001+OksLan@users.noreply.github.com>
2025-01-29 16:33:08 +0400

    29/01 (3)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,11 +3,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   const toggleButton = document.querySelector('.user-nav__toggle');
   const navList = document.querySelector('.main-nav');
-  const navListItems = document.querySelector('.main-nav__list');
+  const icon = toggleButton.querySelector('use');
 
   toggleButton.addEventListener('click', () => {
     toggleButton.classList.toggle('user-nav__toggle--open');
     toggleButton.classList.toggle('user-nav__toggle--close');
     navList.classList.toggle('main-nav--opened');
+
+    // Инвертируем условие
+    const isOpen = navList.classList.contains('main-nav--opened');
+    icon.setAttribute('href', isOpen ? 'icons/stack.svg#cross' : 'icons/stack.svg#burger');
   });
 });
